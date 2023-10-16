24b159bf1253893027915a75e2be16f8a01b4a47
mdlolo <locomd@pm.me>
2023-10-16 13:34:57 -0400

    BLOG QUESTION 1 and extreme commenting fichiers API fecthers

--- blog_website/milestone1/_posts/2023-10-06-data_acquisition.md
+++ blog_website/milestone1/_posts/2023-10-06-data_acquisition.md
@@ -1,11 +1,90 @@
 ---
 layout: post
-author: loic mandine
+author: Equipe A07
 title: Brief Tutorial on Data Acquisition
 ---
-# Donnees play-by-play pour la saison régulière et les playoffs.
+# Telecharger les donnees play-by-play
 
-architecture OOP
+La logique de telechargement des donnees est completement gere par le fichier `nhl_rest_api_fetcher.py`
 
+## How to use it
 
-toto toto
+* First of all, prepare our env by installing the requirements:
+```bash
+python -m venv venv_NHL_API_fetcher
+pip install -r requirements.txt
+```
+
+* Then, you need to tweak the values inside the `.env` file.
+```bash 
+# .env file example
+DATA_FOLDER = './data/' # folder where data will be stored
+LOGGING_FILE = './log_file/' # folder where logs file will be stored
+```
+
+* Then, you can run the script with the following command:
+```bash
+python nhl_rest_api_fetcher.py
+```
+
+This is simple as that! 
+
+The script will download for each season between 2016-2020, every plays of every playoffs and regular-season games.
+
+The resulting data will be stored in the `DATA_FOLDER` folder, in the following structure:
+```
+data
+├── 2016
+│   ├── 2016030001.json
+│   ├── 2016030002.json
+...
+├── 2017
+│   ├── 2017030001.json
+│   ├── 2017030002.json
+...
+
+```
+where each file is a json file containing every play-by-play data of a game.
+If a json file already exists, the script will not download it again, so you can run the script multiple times without worrying about downloading the same data twice.
+
+When run for the first time, the script will download around 4.3GB of json files.
+And take about 10min to run (on a OMEN HP laptop and a bad WIFI connection).
+
+Besides this, the script will also outputs logging files in the `log_file` folder, so you can check how good downloads happened.
+
+## How it works
+
+>We really thoroughly commented and explains every part of the code, as much as we can, 
+>    so we hope that by checking the code with a global overlook will be sufficient for more details.
+
+But here is the header paragraph of the script giving a brief overview of the script scructure:
+
+```python
+"""
+Inspired from the general structure of an URL:
+    scheme://netloc/path;parameters?query#fragment.
+In our case : 
+    https://statsapi.web.nhl.com/api/v1/subpath?query.  
+
+We choose an OOP approach to build the NHL REST API Fetcher objects, 
+    with in mind a Base_Fetcher Class that will be inherited by other classes
+    that corresponds to the different TYPES of endpoints of the NHL REST API.
+
+For now, we have only leveraged two types of endpoints so we have two subclasses :
+    - Game_endpoints_Fetcher
+    - Schedule_endpoints_Fetcher
+
+Mainly, the Base Class allows to factor out 
+    the common attributes (such as part of urls that stays the same) 
+    and methods (such as the one that actually make the GET request).
+
+Especially, we delegate the verification of the endpoint path to the subclasses.
+    Each subclass must a variable ALLOWED_ENDPOINT_PATH that is a regex that
+    will be used to verify the endpoint path provided by the user.
+
+This permits us to have a more flexible code that can be easily extended to other types of endpoints
+if the following Milestones would require it.
+
+For now, the script just needs an external config system (Hydra or CLI Interface) to be more flexible regarding Execution Parameters...
+"""
+```
--- nhl_rest_api_fetcher.py
+++ nhl_rest_api_fetcher.py
@@ -1,50 +1,55 @@
 """
-general structure of a URL: 
-    scheme://netloc/path;parameters?query#fragment. 
-Each tuple item is a string, possibly empty. 
-    
+Inspired from the general structure of an URL:
+    scheme://netloc/path;parameters?query#fragment.
 In our case : 
-    https://statsapi.web.nhl.com/api/v1/subpath?query. 
+    https://statsapi.web.nhl.com/api/v1/subpath?query.  
+
+We choose an OOP approach to build the NHL REST API Fetcher objects, 
+    with in mind a Base_Fetcher Class that will be inherited by other classes
+    that corresponds to the different TYPES of endpoints of the NHL REST API.
+
+For now, we have only leveraged two types of endpoints so we have two subclasses :
+    - Game_endpoints_Fetcher
+    - Schedule_endpoints_Fetcher
+
+Mainly, the Base Class allows to factor out 
+    the common attributes (such as part of urls that stays the same) 
+    and methods (such as the one that actually make the GET request).
+
+Especially, we delegate the verification of the endpoint path to the subclasses.
+    Each subclass must a variable ALLOWED_ENDPOINT_PATH that is a regex that
+    will be used to verify the endpoint path provided by the user.
+
+This permits us to have a more flexible code that can be easily extended to other types of endpoints
+if the following Milestones would require it.
+
+For now, the script just needs an external config system (Hydra or CLI Interface) to be more flexible regarding Execution Parameters...
 """
 
-import datetime
 import json
+import os
 import urllib
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Dict, Literal, Tuple, Union
 import requests
-
 import urllib3
-
 from pregex.core.quantifiers import Optional
 from pregex.core.groups import Capture
 from pregex.core.operators import Either
-from pregex.meta.essentials import Integer
-from pregex.core.classes import AnyLetter, AnyDigit
-from pregex.core.assertions import MatchAtEnd, MatchAtStart, EnclosedBy
-
+from pregex.core.classes import AnyDigit
+from pregex.core.assertions import MatchAtEnd, MatchAtStart
+from dotenv import load_dotenv
 from loguru import logger
-
-def generate_url_path(*parts, use_posix=False):
-    path = Path(*parts)
-    return path.as_posix() if use_posix else str(path)
-
-logger.add(
-    "log_file/data_acquisition.log",
-    compression="zip",
-    level="DEBUG",
-    format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> <green>{elapsed}</green> | <level>{level: <8}</level> | <cyan>{file}</cyan>:<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-)
-
-# Remove print on stdout (---> hide the progress bars)
-logger.remove(0)
-
 
 @dataclass
 class Base_Fetcher:
+    '''
+    Base class that will be inherited by the other classes.
+    One important method is the `fetch()` method that will make the GET request
+    '''
 
-    # URL components
+    # BASE attributes for common part of the url
     path: str
     query: Dict[str, str]
     scheme: str = "https"
@@ -57,9 +62,24 @@ class Base_Fetcher:
 
     def fetch(
         self, 
-        output_format: str = "json",
+        output_format: Literal["json", "binary", "text", "raw"] = "json",
         save_local: str = None,
     ) -> Union[dict, bytes, str, urllib3.response.HTTPResponse]:
+        """Function that makes the GET request to an NHL REST API endpoint path
+            and return the response in the format specified by the user (in our case will mainly be json).
+
+            if save_local is not None, the response will be saved locally at the path specified by the user.
+                and if save_local is not None and the file already exists, the GET request will be skipped.
+        Args:
+            output_format (str, optional): output format of the GET Request. Defaults to "json".
+            save_local (str, optional): path to save json file, if already. Defaults to None.
+
+        Raises:
+            RuntimeError: if output_format is one of ["json", "binary", "text", "raw"]
+
+        Returns:
+            Union[dict, bytes, str, urllib3.response.HTTPResponse]: in our case will mainly be json --> so returning a dict
+        """        ''''''
         
         if output_format not in self.output_format:
             raise RuntimeError(
@@ -102,6 +122,15 @@ class Base_Fetcher:
         return self.response
 
     def _read_content_from_local(self, save_local: str, output_format: str):
+
+        """In case the json file already exists locally, function is called by `fectch()` to read the content of the file
+
+        Raises:
+            NotImplementedError: if the user provides an output_format that we don't to read from locally
+
+        Returns:
+            _type_: dict, bytes, str
+        """        
         if output_format == "json":
             with open(save_local, "r") as file:
                 return json.load(file)
@@ -119,7 +148,15 @@ class Base_Fetcher:
                 "DONT KNOW HOT TO SERIALIZE A urllib3.response.HTTPResponse PYTHON OBJECT"
             )
 
-    def _save_local(self, path):
+    def _save_local(self, path : str):
+        """Save locally the response of the GET request
+
+        Args:
+            path (str): path to save locally te file
+
+        Raises:
+            NotImplementedError: dont know how to serialize locally a urllib3.response.HTTPResponse object
+        """        
         if isinstance(self.response, dict):
             with open(path, "w") as file:
                 json.dump(self.response, file, indent=4)
@@ -139,21 +176,36 @@ class Base_Fetcher:
 
 
 class Game_endpoints_Fetcher(Base_Fetcher):
+    """SubClass of Base_Fetcher that will be used to fetch GAME-RELATED endpoints from the NHL REST API
+
+    Args:
+        Base_Fetcher (_type_): Base class implementing fetch() method and other useful attributes
+
+    Raises:
+        RuntimeError: if the user provides a bad endpoint path that does not match the regex self.ALLOWED_ENDPOINT_PATH
+        RuntimeError: if the user provides a bad game_id that does not match the regex self.GAME_ID
+        RuntimeError: if the user specified the endpoint /feed/live/diffPatch without specifying query parameters
+    """    
 
     """
-    TODO: Retravailler le premier groupe de la regex qui controle lannee de la saison
-    pour cette condition 1917 <= 4_premiers_digits <= current_year
+    To understand more on the verification done in the below REGEX
+    go here https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-endpoints
     """
 
+    
+    # TODO: Retravailler le premier groupe de la regex qui controle lannee de la saison
+    # pour respect cette condition : 1917 <= 4_premiers_digits <= current_year
+    
     GAME_ID = (
         MatchAtStart(Capture(4 * AnyDigit()))
         + Capture("0" + Either("1", "2", "3", "4"))
         + MatchAtEnd(Capture(4 * AnyDigit()))
     )
 
-    """
-        TODO: Retravailler la regex GAME_ID ci-dessus car il ne peut pas y avoir MatchAtEnd ou MatchAtStart
-    """
+    
+    # TODO: Retravailler la regex GAME_ID ci-dessus car il ne peut pas y avoir MatchAtEnd ou MatchAtStart
+    # et l'utiliser dans la regex en dessous
+    
     ALLOWED_ENDPOINT_PATH = Either(
         MatchAtStart("game/")
             + 4 * AnyDigit()
@@ -177,8 +229,15 @@ class Game_endpoints_Fetcher(Base_Fetcher):
 
     def __init__(self, path: str, query_parameters: Dict[str, str] = None):
         """
-        To understand more on the verification done in this function
+        To understand more on the verification done here
         go here https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-endpoints
+
+        Args:
+            path (str): ENDPOINT PATH to fetch of the NHL REST API
+            query_parameters (Dict[str, str], optional): Parameter for the GET Request. Defaults to None.
+
+        Raises:
+            RuntimeError: if `path` == '/feed/live/diffPatch' and `query_parameters` == None
         """
         super().__init__(path, query_parameters)
 
@@ -208,6 +267,18 @@ class Game_endpoints_Fetcher(Base_Fetcher):
         self.path = generate_url_path(self.root_path, path, use_posix=True)
 
     def _verify_endpoint_path(self, endpoint_path: str) -> None:
+        """Verify if self.ALLOWED_ENDPOINT_PATH matches the endpoint path provided by the user
+        
+        if match happened successfully we add a class attribute which is 
+            the subpath of the game endpoint in `self.subpath_if_game_endpoint`
+            i.e. in '/game/{gameid}/feed/live' the subpath is '/feed/live'
+
+        Args:
+            endpoint_path (str): endpoint path provided by the user
+
+        Raises:
+            RuntimeError: if the user provides a bad endpoint path that does not match the regex self.ALLOWED_ENDPOINT_PATH
+        """        
         if self.ALLOWED_ENDPOINT_PATH.get_matches(endpoint_path) != []:
             logger.info(
                 f"Endpoint path provided by user is correct : {self.root_path}/{endpoint_path} "
@@ -220,13 +291,26 @@ class Game_endpoints_Fetcher(Base_Fetcher):
         else:
             raise RuntimeError(
                 f"""
-                                YOU PROVIDED A BAD ENDPOINT PATH {endpoint_path}
-                                SEE MORE information on https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-endpoints
-                                CAN NOT CONTINUE THE GET REQUEST
-                                """
+                YOU PROVIDED A BAD ENDPOINT PATH {endpoint_path}
+                SEE MORE information on https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-endpoints
+                CAN NOT CONTINUE THE GET REQUEST
+                """
             )
 
-    def _verify_game_id(self, game_id) -> None:
+    def _verify_game_id(self, game_id : str) -> None:
+        """Verify if the game_id provided by the user is correct
+        Called only if the user specified an endpoint path as followed '/game/{game_id}'
+
+        if match happened successfully we add a class attribute which is 
+            the 3 components of gameId in respectively self.season_of_the_game, self.type_of_game, self.game_number
+            i.e. in '2016020001' the 3 components are ('2016', '02', '0001')
+        
+        Args:
+            game_id (str): game id provided by the user
+
+        Raises:
+            RuntimeError: if the user provides a bad game_id that does not match the regex self.GAME_ID
+        """            
         if self.GAME_ID.get_matches(game_id) != []:
             logger.info(f"Game id provided by user {game_id} is correct")
             (
@@ -237,23 +321,28 @@ class Game_endpoints_Fetcher(Base_Fetcher):
         else:
             raise RuntimeError(
                 f"""
-                                YOU PROVIDED A BAD GAME_ID {game_id}
-                                SEE MORE information on https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-ids
-                                CAN NOT CONTINUE THE GET REQUEST
-                                """
+                YOU PROVIDED A BAD GAME_ID {game_id}
+                SEE MORE information on https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-ids
+                CAN NOT CONTINUE THE GET REQUEST
+                """
             )
 
 
 class Schedule_endpoints_Fetcher(Base_Fetcher):
+    """SubClass of Base_Fetcher that will be used to fetch SCHEDULE-RELATED endpoints from the NHL REST AP
 
-    """
-    TODO: Ecrire une regex qui verifie https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#schedule-modifiers
-    dans les query ajoute par l'user
-    """
+    Args:
+        Base_Fetcher (_type_): Base class implementing fetch() method and other useful attributes
 
-    """
-        TODO: Retravailler la Optional regex pour le cas 'seasons' pour preciser l'ecriture des annees 
-    """
+    Raises:
+        RuntimeError: if the user provides a bad endpoint path that does not match the regex self.ALLOWED_ENDPOINT_PATH
+    """    
+
+
+    # TODO: Ecrire une regex qui verifie https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#schedule-modifiers
+    # dans les query ajoute par l'user
+    # TODO: Retravailler la Optional regex pour le cas 'seasons' pour imposer une ecriture des annees 
+    
     ALLOWED_ENDPOINT_PATH = Either(
         MatchAtEnd(MatchAtStart("schedule")),
         MatchAtStart("seasons")
@@ -261,10 +350,14 @@ class Schedule_endpoints_Fetcher(Base_Fetcher):
     )
 
     def __init__(self, path: str, query_parameters: Dict[str, str] = None):
-        """
+        """Schedule_endpoints_Fetcher constructor
         To understand more on the verification done in this function
         go here https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#game-endpoints
-        """
+
+        Args:
+            path (str): ENDPOINT PATH to fetch of the NHL REST API
+            query_parameters (Dict[str, str], optional): Parameter for the GET Request . Defaults to None.
+        """        
         super().__init__(path, query_parameters)
 
         self._verify_endpoint_path(path)
@@ -283,16 +376,26 @@ class Schedule_endpoints_Fetcher(Base_Fetcher):
         else:
             raise RuntimeError(
                 f"""
-                                YOU PROVIDED A BAD ENDPOINT PATH {endpoint_path}
-                                SEE MORE information on https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#schedule-endpoints
-                                CAN NOT CONTINUE THE GET REQUEST
-                                """
+                YOU PROVIDED A BAD ENDPOINT PATH {endpoint_path}
+                SEE MORE information on https://gitlab.com/dword4/nhlapi/-/blob/master/stats-api.md?ref_type=heads#schedule-endpoints
+                CAN NOT CONTINUE THE GET REQUEST
+                """
             )
 
 
 def regular_playoff_p_by_p_data_per_season():
+    '''
+    For now, Main Entrypoint of the script.
+    Will instantiate the Schedule_endpoints_Fetcher and Game_endpoints_Fetcher classes
+    in order to fetch the data from the NHL REST API and save them locally.
+
+
+    '''
     from tqdm import tqdm, trange
 
+    #TODO : Refactor list of years using a config file. 
+    #       Generally make this file more flexible by using external config files
+    # such as with Hydra library OR with a CLI Interface
     for year in trange(2016, 2021, desc=f"Iterating on year"):
         
         response = Schedule_endpoints_Fetcher(
@@ -307,7 +410,7 @@ def regular_playoff_p_by_p_data_per_season():
 
         with tqdm(total=len(game_ids)) as pbar:
             for id in game_ids:
-                path_save_local = Path("./data") / str(year) / f"{id}.json"
+                path_save_local = Path(os.getenv('DATA_FOLDER')) / str(year) / f"{id}.json"
                 path_save_local.parent.mkdir(parents=True, exist_ok=True)
                 pbar.set_description(
                     f"Fetching game/{id}/feed/live saved at {path_save_local}"
@@ -318,5 +421,33 @@ def regular_playoff_p_by_p_data_per_season():
                 )
 
 
+def generate_url_path(*parts, use_posix=False):
+    '''
+    Emerges from the problem of running this script on Windows and Linux.
+    Needed an OS-agnostic way to generate an url path (mainly used in the GET request endpoint path)
+    '''
+    path = Path(*parts)
+    return path.as_posix() if use_posix else str(path)
+
+def init_logger():
+    '''
+    Initialize the logger that will log into some files and not on stdout
+    We use the loguru library so it takes care of the rotation/renaming of the logs file 
+        and offer less boilerplate code than the standard logging library...
+    '''
+    logger.add(
+        Path(os.getenv('LOGGING_FILE')) / "data_acquisition.log",
+        compression="zip",
+        level="DEBUG",
+        format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> <green>{elapsed}</green> | <level>{level: <8}</level> | <cyan>{file}</cyan>:<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    )
+
+    # Remove print on stdout (otherwise hide the progress bars)
+    logger.remove(0)
+
 if __name__ == "__main__":
-    regular_playoff_p_by_p_data_per_season()
+    if load_dotenv('.env',verbose=True):
+        init_logger()
+        regular_playoff_p_by_p_data_per_season()
+    else:
+        raise RuntimeError("COULD NOT LOAD THE .env FILE")
